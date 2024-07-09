66a64def6f15e61097f795c44d29095dd889cf2d
Sunny Aitawade <sunny.aitawade@gmail.com>
2024-07-09 13:13:14 +0530

    First Commit: Stream API questions are added

---
+++ core-java/stream-api/src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * @author Avaruus Studios
+ * @name ${NAME}
+ * @date 7/9/2024
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        StudentService studentService = new StudentService();
+        studentService.findFirstNameWithAChar();
+    }
+}
